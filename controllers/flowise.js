ab8b7dafeb8c49caf92a43a11a5fb569085ce5b3
williammyq <397557446@qq.com>
2024-01-30 18:55:08 +0800

    first commmit

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -7,8 +7,21 @@ export const createPrediction = async (req, res) => {
     const flowiseData = {
       question: message,
     };
+    const response = await fetch(
+      `${process.env.FLOWISE_URL}/api/v1/prediction/${process.env.FLOW_ID}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.FLOWISE_API_KEY}`
+        },
+        body: JSON.stringify(flowiseData),
+      }
+    );
+    const data = await response.json();
+    console.log(data);
 
-    res.status(200).json({ message: "Demo Response" });
+    res.status(200).json({ message: data.text });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
